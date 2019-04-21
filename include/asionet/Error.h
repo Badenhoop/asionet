91f80bfcd262a055a3f5b209d682f5503e9765bb
Hoop77 <p.badenhoop@gmx.de>
2019-04-21 13:11:50 +0200

    making error codes constexpr

--- include/asionet/Error.h
+++ include/asionet/Error.h
@@ -53,22 +53,22 @@ struct Error
 	{ return !(lhs == rhs); }
 };
 
-namespace codes { ErrorCode success{0}; }
+namespace codes { constexpr ErrorCode success{0}; }
 const Error success{codes::success};
 
-namespace codes { ErrorCode failedOperation{1}; }
+namespace codes { constexpr ErrorCode failedOperation{1}; }
 const Error failedOperation{codes::failedOperation};
 
-namespace codes { ErrorCode aborted{2}; }
+namespace codes { constexpr ErrorCode aborted{2}; }
 const Error aborted{codes::aborted};
 
-namespace codes { ErrorCode encoding{3}; }
+namespace codes { constexpr ErrorCode encoding{3}; }
 const Error encoding{codes::encoding};
 
-namespace codes { ErrorCode decoding{4}; }
+namespace codes { constexpr ErrorCode decoding{4}; }
 const Error decoding{codes::decoding};
 
-namespace codes { ErrorCode invalidFrame{5}; }
+namespace codes { constexpr ErrorCode invalidFrame{5}; }
 const Error invalidFrame{codes::invalidFrame};
 
 }
